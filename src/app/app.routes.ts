1a1ed87df6c5e477acfa24db5f91ff7edaa50105
Damien Vandendorpe <damienvdh59@gmail.com>
2026-04-06 21:58:54 +0200

    feat: home page hero + intro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { SkillsComponent } from './pages/skills/skills.component'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'about', component: AboutComponent},
     { path: 'contact', component: ContactComponent},
+    { path: "skills", component: SkillsComponent},
     { path: '**', redirectTo: '' },
 ];
